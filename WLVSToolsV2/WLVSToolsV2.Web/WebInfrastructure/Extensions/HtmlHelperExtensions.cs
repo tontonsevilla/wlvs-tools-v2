77622b52d50693f7a721538a47b5767b43ba6482
Winston Sevilla <wsevilla@blastasia.com>
2026-02-23 20:52:12 +0800

    update: layout for Areas

---
+++ WLVSToolsV2/WLVSToolsV2.Web/WebInfrastructure/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WLVSToolsV2.Web.WebInfrastructure.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string GetVersion()
+        {
+            return string.Empty;
+        }
+    }
+}
